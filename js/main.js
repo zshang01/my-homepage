6a632a75cf7bdd9a16818801e26d2ca53b948f8c
Zhou Shang <alex.zhou.shang>
2019-01-30 12:39:10 -0800

    change

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@ document.getElementById('submit').addEventListener('click',
         var email = document.querySelector('#Email').value;
         var msg = document.querySelector('#MSG').value;
 
-        if(re.test(firstName) === true){
+        if(!re.test(firstName) === true){
             confirm("first name is wrong");
         }
         if(!re.test(lastName) === true){
             confirm("last name is wrong");
         }
-        if(at.test(email) === true){
+        if(!at.test(email) === true){
             confirm("email is wrong");
         }
         if(msg.length === 0){
